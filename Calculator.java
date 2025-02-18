b8d3db3bc424b3fd6890ce6f9f559df9122778d3
Shabbeer Petersen <shabbeer.petersen@capaciti.org.za>
2025-02-18 15:43:06 +0200

    added validity check on operator input

--- Calculator.java
+++ Calculator.java
@@ -6,26 +6,37 @@ public class Calculator {
             System.out.print("Enter first number: ");
             double num1 = scanner.nextDouble();
 
-            System.out.println("Enter second number: ");
+            System.out.print("Enter second number: ");
             double num2 = scanner.nextDouble();
-
-            System.out.print("Enter an operator (+, -, *, /)");
-            char operator = scanner.next().charAt(0);
 
             double result = 0;
 
-            switch (operator) {
-                case '+' -> result = num1 + num2;
-                case '-' -> result = num1 - num2;
-                case '*' -> result = num1 * num2;
-                case '/' -> {
-                    if (num2 != 0) {
-                        result = num1 / num2;
-                    } else{
-                        System.out.println("Error! Division by zero is not allowed.");
+            boolean valid = false;
+            do{
+                System.out.print("Enter an operator (+, -, *, /): ");
+                char operator = scanner.next().charAt(0);
+
+                switch (operator) {
+                    case '+' -> {
+                        result = num1 + num2;
+                        valid = true;}
+                    case '-' -> {
+                        result = num1 - num2;
+                        valid = true;}
+                    case '*' -> {
+                        result = num1 * num2;
+                        valid = true;}
+                    case '/' -> {
+                        if (num2 != 0) {
+                            result = num1 / num2;
+                            valid = true;
+                        } else{
+                            System.out.println("Error! Division by zero is not allowed.");
+                        }
                     }
+                    default -> System.out.println("\nError! Invalid operator. Try again.\n");
                 }
-            }
+            }while (!valid);
 
             System.out.println("The result is: " + result);
         }
